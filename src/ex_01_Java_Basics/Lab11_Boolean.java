6bece4babbfbe4645f611edf3788d6e72dcdcba5
Sachin <sachinairo998@gmail.com>
2025-09-14 18:22:25 +0530

    14 Sep 2025 Part 1,2

--- src/ex_01_Java_Basics/Lab11_Boolean.java
+++ src/ex_01_Java_Basics/Lab11_Boolean.java
@@ -2,9 +2,9 @@ package ex_01_Java_Basics;
 
 public class Lab11_Boolean {
     public static void main(String[] args) {
-        boolean 1 = True;
+/*        boolean 1 = True;
         boolean 2 = False;
-        boolean = 30;
-//        boolean value not accepted only True or False
+        boolean = 30;*/
+//        boolean numerical value not accepted only True or False
     }
 }
